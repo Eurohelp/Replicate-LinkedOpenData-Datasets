364323ea1de3229cbc84b446eca9ea8ded717db7
Mishel Uchuari <dmuv7@hotmail.com>
2017-11-13 12:01:12 +0100

    añadida funcionalidad creacion csv urumea
    close #1

---
+++ CSVToRDFUrumea/src/tranformation/Preprocess.java
@@ -0,0 +1,37 @@
+package tranformation;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Preprocess {
+	private String initialCsvName;
+	private char delimiter;
+
+	/**
+	 * Path csv a convertir
+	 * @param pInitialName
+	 */
+	public Preprocess(String pInitialName) {
+		initialCsvName = pInitialName;
+		delimiter = ',';
+	}
+
+	public void CSVpreprocess() throws IOException {
+		CSV csv = new CSV(initialCsvName, delimiter);
+		// Creamos una nueva columna en la que los nombres de las calles estarán
+		// separadas por un guion
+		List<String> dates = csv.getColumnValue("eguna");
+		List<String> hours = csv.getColumnValue("ordua");
+		List<String> datetimes = Transformation.getInstance().putDateTimeFormat(dates, hours);
+		csv.addColumn("horafecha", datetimes);
+		hours = Transformation.getInstance().urlify(hours);
+		csv.addColumn("horaParaUri", hours);
+		List<String> level = csv.getColumnValue("maila");
+		level = Transformation.getInstance().replace(level, "m", "");
+		csv.addColumn("maila", level);
+		csv.saveCSV();
+	}
+}
