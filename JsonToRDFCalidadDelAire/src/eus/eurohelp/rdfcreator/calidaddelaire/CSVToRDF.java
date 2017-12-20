42c3ad79d6bb31d115d518fa10e1286936cf1a92
Mishel Uchuari <dmuv7@hotmail.com>
2017-12-20 13:31:23 +0100

    cambio nombre carpeta

---
+++ JsonToRDFCalidadDelAire/src/eus/eurohelp/rdfcreator/calidaddelaire/CSVToRDF.java
@@ -0,0 +1,68 @@
+package eus.eurohelp.rdfcreator.calidaddelaire;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import org.openrdf.repository.Repository;
+import org.openrdf.rio.RDFFormat;
+
+import be.ugent.mmlab.rml.core.RMLEngine;
+import be.ugent.mmlab.rml.core.StdRMLEngine;
+import be.ugent.mmlab.rml.mapdochandler.extraction.std.StdRMLMappingFactory;
+import be.ugent.mmlab.rml.mapdochandler.retrieval.RMLDocRetrieval;
+import be.ugent.mmlab.rml.model.RMLMapping;
+import be.ugent.mmlab.rml.model.dataset.RMLDataset;
+/**
+ * 
+ * @author dmuchuari
+ * @05/12/2017
+ */
+public class CSVToRDF {
+	// En primer lugar se le pasara el path del json
+	// En segundo lugar se le pasara el path del archivo de configuracion
+	// En tercer lugar la ruta donde se quiera almacenar el archivo
+	// resultante con la extensi�n deseada
+	public static void main(String[] args) throws IOException {
+
+		
+		// Ejecuci�n del archivo RML
+		try {
+			File outputFile = Paths.get(args[1]).toFile();
+			File mapping_file = Paths.get(args[0]).toFile();
+			RMLDocRetrieval mapDocRetrieval = new RMLDocRetrieval();
+			Repository repository = mapDocRetrieval.getMappingDoc(mapping_file.toString(), RDFFormat.TURTLE);
+			StdRMLMappingFactory mappingFactory = new StdRMLMappingFactory();
+			if (repository == null) {
+				System.out.println("No se ha generado RDF");
+				throw new Exception("RML CONFIGURATION FILE SYNTAX ERROR->There is some problem with rml configuration file, please check it. Maybe the problem is the sintax");
+			}
+			RMLMapping mapping = mappingFactory.extractRMLMapping(repository);
+
+			String outputFormat = RDFFormat.TURTLE.toString();
+			
+				if (!outputFile.toString().toLowerCase().contains("ttl")) {
+					int init = outputFile.toString().indexOf(".");
+					String newPath = args[1].substring(0, init) + ".ttl";
+					outputFile = Paths.get(newPath).toFile();
+				}
+			
+			RMLEngine engine = new StdRMLEngine(outputFile.toString());
+
+			final RMLDataset runningDataset = engine.chooseSesameDataSet("dataset", outputFile.toString(),
+					outputFormat);
+			engine.runRMLMapping(runningDataset, mapping, "", null, null);
+
+			runningDataset.closeRepository();
+
+			if (outputFile.length() == 0) {
+				System.out.println("No se ha generado RDF");
+				throw new Exception("ARGUMENTS ERROR->There is some problem with RDF Generation. Please check the program arguments \n");
+			}
+			Thread.sleep(2000);
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+	}
+}
---
+++ JsonToRDFCalidadDelAire/src/eus/eurohelp/rdfcreator/mongodb/MongoDBUtils.java
@@ -0,0 +1,69 @@
+package eus.eurohelp.rdfcreator.mongodb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.bson.Document;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ * 
+ * @author dmuchuari 
+ * @14/11/2017
+ */
+
+public class MongoDBUtils {
+
+	private MongoDatabase dataBase;
+	private MongoClient mongoClient;
+	private MongoCollection<Document> collection;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param pDataBase
+	 * @param pCollection
+	 * @throws IOException
+	 */
+	public MongoDBUtils(String pDataBase, String pCollection) throws IOException {
+		String mongoPath = PropertiesManager.getINSTANCE().getProperty("mongodb-client");
+		String mongoPort = PropertiesManager.getINSTANCE().getProperty("mongodb-port");
+		mongoClient = new MongoClient(mongoPath, Integer.parseInt(mongoPort));
+		dataBase = mongoClient.getDatabase(pDataBase);
+		collection = dataBase.getCollection(pCollection);
+	}
+
+	/**
+	 * Obtiene los documento de la coleccion "iot_environment_measurements" que
+	 * cumplan la expresion regular por id
+	 * 
+	 * @param pRegex
+	 * @param pPath
+	 * @throws Exception 
+	 */
+	public void getDocument(String pRegex, String pPath) throws Exception {
+		BasicDBObject regex = new BasicDBObject();
+		int cont = 0;
+		PrintWriter pw;
+		regex.put("_id", pRegex);
+		FindIterable<Document> cursor = collection.find(regex);
+		MongoCursor<Document> iterator = cursor.iterator();
+		
+		while (iterator.hasNext()) {
+			pw = new PrintWriter(pPath.replace(".json", "-" + cont + ".json"));
+			Document d = iterator.next();
+			cont++;
+			pw.println(d.toJson());
+			pw.close();
+		}
+		if(cont<=0){
+			throw new Exception("No se encuentran datos con ese patron");
+		}
+	}
+}
